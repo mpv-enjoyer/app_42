a379a5664340a0e50fb89b691c0d748c1d68b859
Alex <Disblocker.just@gmail.com>
2025-02-15 16:09:45 +0300

    compile with drogon framework

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM gcc:latest
-RUN apt-get update
-RUN apt-get install -y cmake
+FROM drogonframework/drogon:latest
 COPY . /usr/src/app_42
 WORKDIR /usr/src/app_42
 RUN mkdir build
 WORKDIR /usr/src/app_42/build
 RUN cmake ..
 RUN make
-CMD ["./app", "google.com"]
+EXPOSE 8848
+CMD ["./app_42", "google.com"]
 LABEL Name=app_42 Version=0.0.1
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,45 @@
-#include "server.h"
+#include <trantor/utils/Logger.h>
+#ifdef _WIN32
+#include <ws2tcpip.h>
+#else
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#endif
 
-int main(int argc, char *argv[])
+#include <drogon/drogon.h>
+using namespace drogon;
+
+int main()
 {
-    return server_example(argc, argv);
+    // `registerHandler()` adds a handler to the desired path. The handler is
+    // responsible for generating a HTTP response upon an HTTP request being
+    // sent to Drogon
+    app().registerHandler(
+        "/",
+        [](const HttpRequestPtr &request,
+           std::function<void(const HttpResponsePtr &)> &&callback) {
+            LOG_INFO << "connected:"
+                     << (request->connected() ? "true" : "false");
+            auto resp = HttpResponse::newHttpResponse();
+            resp->setBody("Hello, World!");
+            callback(resp);
+        },
+        {Get});
+    app()
+        .setBeforeListenSockOptCallback([](int fd) {
+            LOG_INFO << "setBeforeListenSockOptCallback:" << fd;
+#ifdef _WIN32
+#elif __linux__
+            int enable = 1;
+            if (setsockopt(
+                    fd, IPPROTO_TCP, TCP_FASTOPEN, &enable, sizeof(enable)) ==
+                -1)
+            {
+                LOG_INFO << "setsockopt TCP_FASTOPEN failed";
+            }
+#else
+#endif
+        })
+        .setAfterAcceptSockOptCallback([](int) {});
+    app().addListener("127.0.0.1", 8848).run();
 }
