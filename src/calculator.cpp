89609086ad29df890521f561eb7bb189d29c2ec3
Alex <Disblocker.just@gmail.com>
2025-02-16 01:36:21 +0300

    cleanup

---
+++ src/calculator.cpp
@@ -0,0 +1,129 @@
+#include <stack>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+
+#include "calculator.h"
+
+std::optional<double> ReversePolishCalculator::is_numeric(std::string const &str)
+{
+    double result = 0.0;
+    auto i = std::istringstream(str);
+    i >> result;
+    
+    if (!i.fail() && i.eof()) return result;
+    else return std::optional<double>();
+}
+
+ReversePolishCalculator::ReversePolishCalculator(std::string expression)
+{
+    auto i = std::istringstream(expression);
+    std::string word;
+    std::stack<double> values;
+    while (i >> word)
+    {
+        auto number = is_numeric(word);
+        if (number)
+        {
+            values.push(number.value());
+        }
+        else
+        {
+            double right = values.top();
+            values.pop();
+            double left = values.top();
+            values.pop();
+            if (word == std::string("+")) values.push(left + right);
+            if (word == std::string("-")) values.push(left - right);
+            if (word == std::string("*")) values.push(left * right);
+            if (word == std::string("/")) values.push(left / right);
+        }
+    }
+    if (values.size() == 0)
+    {
+        m_value = 0;
+    }
+    else
+    {
+        m_value = values.top();
+    }
+}
+
+double ReversePolishCalculator::value()
+{
+    return m_value;
+}
+
+int Calculator::get_precedence(std::string op)
+{
+    switch (op[0])
+    {
+    case '(': return 2;
+    case '+': return 1;
+    case '-': return 1;
+    case '*': return 0;
+    case '/': return 0;
+    default: throw std::invalid_argument("Invalid operator");
+    };
+}
+
+std::string Calculator::translate_to_reverse_polish(std::string expression)
+{
+    auto i = std::istringstream(expression);
+    std::string word;
+    std::stack<std::string> operators;
+    std::string output;
+    output.reserve(expression.size());
+    while (i >> word)
+    {
+        auto number = is_numeric(word);
+        if (number)
+        {
+            output.append(std::to_string(number.value()));
+            output.append(" ");
+            continue;
+        }
+        if (word == std::string("("))
+        {
+            operators.push("(");
+            continue;
+        }
+        if (word == std::string(")"))
+        {
+            while (operators.top() != std::string("("))
+            {
+                output.append(operators.top());
+                output.append(" ");
+                operators.pop();
+            }
+            operators.pop();
+            continue;
+        }
+        while (!operators.empty() && get_precedence(operators.top()) <= get_precedence(word))
+        {
+            output.append(operators.top());
+            output.append(" ");
+            operators.pop();
+        }
+        operators.push(word);
+    }
+    while (!operators.empty())
+    {
+        output.append(operators.top());
+        output.append(" ");
+        operators.pop();
+    }
+    return output;
+}
+
+Calculator::Calculator(std::string expression)
+: ReversePolishCalculator(translate_to_reverse_polish(expression)),
+m_translated_expression(translate_to_reverse_polish(expression))
+{
+
+}
+
+std::string Calculator::get_translated() const
+{
+    return m_translated_expression;
+}
