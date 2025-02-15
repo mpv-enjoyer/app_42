2caf57ffd79f00d2b2ce55e457d13eab70681c04
Alex <Disblocker.just@gmail.com>
2025-02-15 20:14:25 +0300

    calculator add() with polish notation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,101 @@
+#include <string>
+#include <sstream>
+#include <optional>
+#include <stack>
+#include <memory>
+#include <iostream>
+
 #include "server.h"
+
+class Operation
+{
+protected:
+    std::optional<double> m_left_value;
+    Operation() { };
+public:
+    void setLeftValue(double left_value)
+    {
+        m_left_value.emplace(left_value);
+    };
+    bool isLeftValueSet() const
+    {
+        return m_left_value.has_value();
+    };
+    virtual double get(double right_value) const = 0;
+};
+
+class Add : public Operation
+{
+public:
+    Add() { };
+    double get(double right_value) const override
+    {
+        return m_left_value.value() + right_value;
+    }
+};
+
+/* 2 + 2 * 2 => + 2 * 2 2 */
+
+class Calculator
+{
+    std::stack<std::unique_ptr<Operation>> m_operations;
+    double m_value = 0;
+    std::optional<double> is_numeric(std::string const& str) 
+    {
+        double result = 0.0;
+        auto i = std::istringstream(str);
+        i >> result;
+        
+        if (!i.fail() && i.eof()) return result;
+        else return std::optional<double>();
+    }
+    bool process_number(double value)
+    {
+        while (!m_operations.empty())
+        {
+            auto& operation = m_operations.top();
+            if (!operation->isLeftValueSet())
+            {
+                operation->setLeftValue(value);
+                return true;
+            }
+            else
+            {
+                value = operation->get(value);
+                m_operations.pop();
+            }
+        }
+        m_value = value;
+        return false;
+    }
+public:
+    Calculator(std::string expression)
+    {
+        auto i = std::istringstream(expression);
+        std::string word;
+        while (i >> word)
+        {
+            auto number = is_numeric(word);
+            if (number)
+            {
+                if (!process_number(number.value())) break;
+            }
+            else
+            {
+                if (word == std::string("+")) m_operations.push(std::make_unique<Add>());
+            }
+        }
+    }
+    double value()
+    {
+        return m_value;
+    };
+};
 
 int main(int argc, char *argv[])
 {
-    return app42::echoServer(argc, argv);
+    std::cout << Calculator("+ 2 + 2 4").value() << "\n";
+    std::cout << Calculator("1").value() << "\n";
+    std::cout << Calculator("+ 42 + 42 0").value() << "\n";
+    //return app42::echoServer(argc, argv);
 }
