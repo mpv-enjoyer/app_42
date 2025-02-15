efc248e773bd0ea91abd4889bfd41df40ba1ba36
Alex <Disblocker.just@gmail.com>
2025-02-15 20:55:39 +0300

    reverse polish notation will be easier to convert

--- src/main.cpp
+++ src/main.cpp
@@ -4,70 +4,13 @@
 #include <stack>
 #include <memory>
 #include <iostream>
+#include <stdexcept>
 
 #include "server.h"
 
-class Operation
-{
-protected:
-    std::optional<double> m_left_value;
-    Operation() { };
-public:
-    void setLeftValue(double left_value)
-    {
-        m_left_value.emplace(left_value);
-    };
-    bool isLeftValueSet() const
-    {
-        return m_left_value.has_value();
-    };
-    virtual double get(double right_value) const = 0;
-};
-
-class Add : public Operation
-{
-public:
-    Add() { };
-    double get(double right_value) const override
-    {
-        return m_left_value.value() + right_value;
-    }
-};
-
-class Subtract : public Operation
-{
-public:
-    Subtract() { };
-    double get(double right_value) const override
-    {
-        return m_left_value.value() - right_value;
-    }
-};
-
-class Product : public Operation
-{
-public:
-    Product() { };
-    double get(double right_value) const override
-    {
-        return m_left_value.value() * right_value;
-    }
-};
-
-class Divide : public Operation
-{
-public:
-    Divide() { };
-    double get(double right_value) const override
-    {
-        return m_left_value.value() / right_value;
-    }
-};
-
-// Polish notation
+// Reverse polish notation
 class Calculator
 {
-    std::stack<std::unique_ptr<Operation>> m_operations;
     double m_value = 0;
     std::optional<double> is_numeric(std::string const& str) 
     {
@@ -78,44 +21,38 @@ class Calculator
         if (!i.fail() && i.eof()) return result;
         else return std::optional<double>();
     }
-    bool process_number(double value)
-    {
-        while (!m_operations.empty())
-        {
-            auto& operation = m_operations.top();
-            if (!operation->isLeftValueSet())
-            {
-                operation->setLeftValue(value);
-                return true;
-            }
-            else
-            {
-                value = operation->get(value);
-                m_operations.pop();
-            }
-        }
-        m_value = value;
-        return false;
-    }
 public:
     Calculator(std::string expression)
     {
         auto i = std::istringstream(expression);
         std::string word;
+        std::stack<double> values;
         while (i >> word)
         {
             auto number = is_numeric(word);
             if (number)
             {
-                if (!process_number(number.value())) break;
+                values.push(number.value());
             }
             else
             {
-                if (word == std::string("+")) m_operations.push(std::make_unique<Add>());
-                if (word == std::string("-")) m_operations.push(std::make_unique<Subtract>());
-                if (word == std::string("*")) m_operations.push(std::make_unique<Product>());
-                if (word == std::string("/")) m_operations.push(std::make_unique<Divide>());
+                double right = values.top();
+                values.pop();
+                double left = values.top();
+                values.pop();
+                if (word == std::string("+")) values.push(left + right);
+                if (word == std::string("-")) values.push(left - right);
+                if (word == std::string("*")) values.push(left * right);
+                if (word == std::string("/")) values.push(left / right);
             }
+        }
+        if (values.size() == 0)
+        {
+            m_value = 0;
+        }
+        else
+        {
+            m_value = values.top();
         }
     }
     double value()
@@ -126,10 +63,10 @@ public:
 
 int main(int argc, char *argv[])
 {
-    std::cout << Calculator("+ 2 + 2 4").value() << "\n";
+    std::cout << Calculator("2 2 + 4 +").value() << "\n";
     std::cout << Calculator("1").value() << "\n";
-    std::cout << Calculator("+ 42 + 42 0").value() << "\n";
-    std::cout << Calculator("+ 2 * 2 2").value() << "\n";
-    std::cout << Calculator("/ 75 + * 4 90 2").value() << "\n";
+    std::cout << Calculator("42 0 + 42 /").value() << "\n";
+    std::cout << Calculator("2 2 * 2 +").value() << "\n";
+    std::cout << Calculator("75 4 / 90 + 2 -").value() << "\n";
     //return app42::echoServer(argc, argv);
 }
